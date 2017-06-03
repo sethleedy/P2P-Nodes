12abb59808d7aed29abe1f108fc25ba846e692d2
sethleedy <Code@SethLeedy.Name>
2017-06-03 14:35:44 -0400

    Stuck on Smoke peerlist class usage
    Also switched to ESLint. Made changes to Sublime linting/formatting.

--- custom_modules/functions.js
+++ custom_modules/functions.js
@@ -1,4 +1,9 @@
 // Useful Functions
+
+// Module Requirements.
+var smoke = require('smokesignal')
+
+// Module
 module.exports = {
     // Check for Internet connectivity.
     checkInternet: function(cb) {
@@ -9,6 +14,38 @@ module.exports = {
                 cb(true);
             }
         })
+    },
+    // Start localhost P2P Smoke for testing purposes of code
+    startupLocalSmoke: function(defaultSeeds = ["127.0.0.1", "13131"]) {
+        console.log("startupLocalSmoke: " + defaultSeeds[0])
+
+        var localNode = smoke.createNode({
+            port: parseInt(defaultSeeds[1]),
+            //address: smoke.localIp("127.0.0.1/255.0.0.0") // Tell it your subnet and it'll figure out the right IP for you
+            //address: smoke.localIp("192.168.0.207") // Tell it your subnet and it'll figure out the right IP for you
+            address: "192.168.0.207"
+                //,seeds: [{port: defaultSeeds[1], address:defaultSeeds[0]}] // the address of a seed (a known node)
+        })
+
+        // listen on network events...
+
+        localNode.on('connect', function() {
+            // Hey, now we have at least one peer!
+
+            // ...and broadcast stuff -- this is an ordinary duplex stream!
+            //localNode.broadcast.write('Cast: LocalNode: HEYO! I\'m here.\n')
+        })
+
+        localNode.on('disconnect', function() {
+            // Bah, all peers gone.
+        })
+
+        // Broadcast is a stream
+        localNode.broadcast.pipe(process.stdout)
+            //process.stdin.pipe(localNode.broadcast).pipe(process.stdout)
+
+        // Start the darn thing
+        localNode.start()
     }
 };
 
@@ -25,3 +62,15 @@ Array.prototype.clean = function(deleteValue) {
     }
     return this;
 };
+
+
+// Start localhost P2P Smoke for testing purposes of code
+// function startupLocalSmoke(defaultSeeds = ["127.0.0.1", "13"]) {
+//     console.log("Made it")
+
+//     var localNode = smoke.createNode({
+//         port: defaultSeeds[1],
+//         address: smoke.localIp(defaultSeeds[0] + '/255.255.255.0'), // Tell it your subnet and it'll figure out the right IP for you
+//         seeds: [] // the address of a seed (a known node)
+//     })
+// }
--- index.js
+++ index.js
@@ -1,77 +1,127 @@
 #!/usr/bin/env nodejs
-// http://stackoverflow.com/questions/21612980/why-is-usr-bin-env-bash-superior-to-bin-bash
+ // http://stackoverflow.com/questions/21612980/why-is-usr-bin-env-bash-superior-to-bin-bash
+
+'use strict'
+
+// Clear screen
+process.stdout.write('\x1Bc');
 
 // Global Requires:
-var os = require( 'os' )
+var os = require('os')
 var smoke = require('smokesignal')
 var _ = require('underscore')
 
 // Personal Functions
 var pfunctions = require('./custom_modules/functions.js')
 
+// Take the passed JSON formatted argument and place it into a handy variable
+var jsonArgs = process.argv
+jsonArgs[2] = JSON.parse(process.argv[2])
+	// Test to see if "startNonLocalP2P" is there. If not, set default to true.
+if (!("startNonLocalP2P" in jsonArgs[2])) {
+	jsonArgs[2]["startNonLocalP2P"] = true
+}
+//console.log(jsonArgs[2]["startNonLocalP2P"])
+//process.exit()
+
 // First let's find the IP's that are not local and use one or more for the P2P config in SmokeSignal.
 // https://nodejs.org/api/os.html#os_os_networkinterfaces
 // http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js
 
-var networkInterfaces = os.networkInterfaces()
-//console.log(networkInterfaces)
+var networkInterfaces = JSON.parse(JSON.stringify(os.networkInterfaces())) // Converts the weird string format to JSON and then to an Object.
+	//console.log(networkInterfaces)
 
-// Remove IPv6 objects in array
-var removedIPv6 = _.each(networkInterfaces, function(IPInfo) {
+// Remove,
+//		IPv6 objects
+//		Local Addresses
+// in array.
+var removedIPv6 = _.each(networkInterfaces, function(value, key, list) { //If list is a JavaScript object, iterator's arguments will be (value, key, list).
+	//console.log(key)
+	//console.log(value[0].address)
 
-	// If a pair of objects...
-	if (IPInfo.length >1 && IPInfo[1].family == "IPv6") {
-		IPInfo.splice(1,1) // remove it from the array
+	// Check each value for IPv6 addresses. Value typically is 1 or 2 in array.
+	var checkForIPv6 = _.reject(value, function(value2) {
+
+		if (value2.family == "IPv6") {
+			return true // TRUE, filter it!
+		}
+		if (value2.internal == true) {
+			return true // TRUE, filter it!
+		}
+	})
+
+	// Replace the current key with the newly change value from above
+	list[key] = checkForIPv6
+
+	// Test and see if the KEY is now empty from the above checkForIPv6 function. Remove it from the object if so.
+	if (list[key].length == 0) {
+		delete list[key]
 	}
-	// If singular...
-	if (IPInfo[0].family == "IPv6") {
-		IPInfo.splice(0,1) // remove it from the array
-	}
+
 });
-
-//console.log(removedIPv6)
-//process.exit()
-
-// With the IP's, we should filter out the local addresses.
-var newIPs = _.filter(networkInterfaces, function(IPs) {
-	// Since the IPs are paired, IPv4 & IPv6, we only need to look at index 0 to see if it is local or "internal".
-	if (IPs.length) {
-		if (IPs[0].internal === false ) {return 1} else {return 0} // Returning 1 means to the _.filter() that we want it.
-	} else {return 0} // Removes the empty index in the array left over from removing the IPv6 above.
-})
-//console.log(newIPs)
-
+var newIPs = removedIPv6
+	//console.log(newIPs)
+	//console.log(newIPs["eth1"][0].address)
 
 // The IPs are now filtered of local addresses & IPv6 addresses.
 // We need to test for Internet connectivity and use that address for the P2P system.
 
-process.exit()
-
 // Loop addresses and init a Smoke on each address, except Seed address if present.
-var node = smoke.createNode({
-  port: 8495,
-  address: smoke.localIp('192.168.2.1/255.255.255.0'), // Tell it your subnet and it'll figure out the right IP for you
-  seeds: [{port: 13, address:'192.168.2.100'}] // the address of a seed (a known node)
+
+// Default Seeds for now.
+var defaultSeeds = ["127.0.0.1", 13131]
+if (jsonArgs[2].startLocalP2P === true) { pfunctions.startupLocalSmoke(defaultSeeds) } // jsonArgs[2].startLocalP2P == true
+
+// Create an object to hold the nodes and information about them
+var nodes = {}
+	// Start the main P2P node here and then start listening for messages.
+_.each(newIPs, function(value1, key1, list1) {
+	_.each(value1, function(value2, key2, list2) {
+
+		nodes["node-" + value2.address] = smoke.createNode({
+			port: 8495,
+			address: value2.address,
+			seeds: [{ address: defaultSeeds[0], port: defaultSeeds[1] }] // the address of a seed (a known node)
+		})
+		console.log("Startup node-" + value2.address)
+
+	})
 })
 
 // listen on network events...
+_.each(nodes, function(node, key, list) {
+	node.on('connect', function() {
+		// Hey, now we have at least one peer!
 
-node.on('connect', function() {
-  // Hey, now we have at least one peer!
+		// ...and broadcast stuff -- this is an ordinary duplex stream!
+		node.broadcast.write(node.id + ': ' + 'Now online on its local LAN IP: ' + node.options.address + '.\n')
+	})
 
-  // ...and broadcast stuff -- this is an ordinary duplex stream!
-  node.broadcast.write('HEYO! I\'m here')
+	// Broadcast is a stream
+	//localNode.broadcast.pipe(process.stdout)
+	//process.stdin.pipe(node.broadcast).pipe(process.stdout)
+
+	// Test: Check peers for messages and send some.
+
+
+	node.on('disconnect', function() {
+		// Bah, all peers gone.
+	})
+
+
+	// Start the darn thing
+	node.start()
+
+	// meh, I'd rather stop it
+	//node.stop()
+
 })
 
-node.on('disconnect', function() {
-  // Bah, all peers gone.
-})
+// Test: Check peers for messages and send some.
+var newPeerList
+	//console.log(nodes)
+var test1 = nodes[Object.keys(nodes)[0]] // Grab first key value in object.
+newPeerList = nodes["node-192.168.0.207"]
+console.log(newPeerList.peerlist)
 
-// Broadcast is a stream
-process.stdin.pipe(node.broadcast).pipe(process.stdout)
-
-// Start the darn thing
-node.start()
-
-// meh, I'd rather stop it
-node.stop()
+process.exit()
